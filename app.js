432c162e34b2ba37b57c1ef36ea27b9f24f2fad8
Nikhil Arora <nikhil@Nikhils-MacBook-Air.local>
2025-04-04 16:10:08 +0530

    Added Check functionality with for loop

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ let box1 = document.querySelector('.box1');
 let box2 = document.querySelector('.box2');
 let box3 = document.querySelector('.box3');
 let box4 = document.querySelector('.box4');
-let btns = [box1,box2,box3,box4];
+let boxes = [box1,box2,box3,box4];
+
+let heading = document.querySelector('.level');
+
 
 
 document.addEventListener("keypress",function(){
@@ -36,12 +39,14 @@ document.addEventListener("keypress",function(){
     }
 })
 
-function levelUp(){
+function levelUp(){  // for machine output
+    userSeq = [];
     level++;
-    let heading = document.querySelector('.level');
     heading.innerText = `Level ${level}`;
     let boxId = Math.floor(Math.random()*4);
-    btnFlash(btns[boxId]);
+    gameSeq.push(boxes[boxId].id);
+    console.log("level:",level," gameSeq:",gameSeq);
+    btnFlash(boxes[boxId]);
 }
 
 function btnFlash(btn){
@@ -49,4 +54,42 @@ function btnFlash(btn){
     setTimeout(()=>{
         btn.classList.remove('white');
     },250);
+}
+
+for(box of boxes){
+    box.addEventListener('click',function(){
+        if(started == true){
+            boxPress(this);
+            
+        }
+    })
+}
+
+function boxPress(box){   //for user input
+    btnFlash(box);
+    userSeq.push(box.id);
+    console.log("userSeq:",userSeq);
+    checkColor(userSeq.length);
+    
+}
+
+function checkColor(userSeqLength){
+    for(let idx = 0;idx<userSeqLength;idx++){
+        // console.log("lvel:",level," ",userSeqLength)
+        // console.log("gameSeqColor:",gameSeq," ",userSeq);
+        console.log(idx)
+        if(gameSeq[idx]===userSeq[idx]){
+            if(idx==level-1){
+                console.log("levelUp Called")
+                setTimeout(()=>{
+                    levelUp();
+                },1000)
+                // break;
+            }
+        }
+        else{
+            heading.innerText = `Game Over, Max Score: ${level}`;
+            // break;
+        }
+    }
 }
