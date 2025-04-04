a22cd0774a2962497729115f04ad14928745b68c
Nikhil Arora <nikhil@Nikhils-MacBook-Air.local>
2025-04-04 09:56:56 +0530

    Added flash functionality on game start

--- app.js
+++ app.js
@@ -5,7 +5,28 @@ let level = 0;
 let started = false;
 
 
-// let btns = ["red","blue","orange","violet"];
+//stop propagation
+let container = document.querySelector('.container'); 
+let upper = document.querySelector('.upper');
+let lower = document.querySelector('.lower');
+container.addEventListener('click',function(event){
+    event.stopPropagation();
+})
+
+upper.addEventListener('click',function(e){
+    e.stopPropagation();
+})
+
+lower.addEventListener('click',function(e){
+    e.stopPropagation();
+})
+
+let box1 = document.querySelector('.box1');
+let box2 = document.querySelector('.box2');
+let box3 = document.querySelector('.box3');
+let box4 = document.querySelector('.box4');
+let btns = [box1,box2,box3,box4];
+
 
 document.addEventListener("keypress",function(){
     if(started==false){
@@ -19,13 +40,13 @@ function levelUp(){
     level++;
     let heading = document.querySelector('.level');
     heading.innerText = `Level ${level}`;
-    // let boxId = Math.floor(Math.random()*4);
+    let boxId = Math.floor(Math.random()*4);
     btnFlash(btns[boxId]);
 }
 
-function btnFlash(btn){~
+function btnFlash(btn){
     btn.classList.add('white');
     setTimeout(()=>{
         btn.classList.remove('white');
-    },300);
+    },250);
 }
